8b1ada95a4e7e00fce6b52e081c5f687d137e994
Cabruh <mattcabpro@gmail.com>
2025-01-29 02:03:16 -0500

    "refactored into classes"

--- scripts/main.js
+++ scripts/main.js
@@ -1,86 +1,104 @@
-const $bookshelf = document.getElementById("bookshelf")
+const $bookshelfelem=document.getElementById("bookshelf");
+const $libraryheadelem=document.getElementById("libraryhead");
+const $showformelem=document.getElementById("showform");
+const $closeformelem=document.getElementById("closeform");
+const $addbuttonelem=document.getElementById("addbook");
+const $addbookformelem=document.getElementById("addbookform");
+const $bookcopy = document.querySelector(".book");
+const $inputelements=document.querySelectorAll(".forminput");
 
-const mybookshelf = new Bookshelf("Cabruh", $bookshelf);
+class BookshelfUI {
 
-const book1 = new Book("The Lion, the Witch, and the Wardrobe", "C.S. Lewis", 300, true, mybookshelf);
-const book2 = new Book("The Very Hungry Caterpillar", "Eric Carle", 34, false, mybookshelf);
+  bookshelfarr=[];
+  static bookshelfUI=$bookshelfelem;
+  static libraryheader=$libraryheadelem;
+  static showform=$showformelem;
+  static closeform=$closeformelem;
+  static addbook=$addbuttonelem;
+  static addbookform=$addbookformelem;
+  static inputfields=$inputelements;
 
-function Bookshelf(user, element) {
-  this.user=user;
-  this.element=element;
-  this.libraryheader=document.getElementById("libraryhead");
-  this.libraryheader.innerHTML=user +"'s Library";
-  this.showForm=document.getElementById("showform");
-  this.showForm.addEventListener("click", () => {
-    this.addbookform.classList.remove("hidden")
-  });
-  this.closeButton=document.getElementById("closeform");
-  this.closeButton.addEventListener("click", (event) => {
-    this.addbookform.classList.add("hidden");
-    event.preventDefault();
-  });
-  this.addButton=document.getElementById("addbook");
-  this.addButton.addEventListener("click", (event) => {
-    inputfields=document.querySelectorAll(".forminput");
-    inputarray=Array.from(inputfields)
-    bookargs=inputarray.map((node) => node.id !== "readcheck" ? node.value : node.checked);
-    book=new Book(bookargs[0], bookargs[1], bookargs[2], bookargs[3], this);
-    this.addBookToLibrary(book);
-    event.preventDefault();
-  }); 
-  this.addbookform=document.getElementById("addbookform");
-  this.bookshelf=[];
-  this.bookshelfelem=document.getElementById("bookshelf");
-}
-
-function Book(title, author, numPages, read, library) {
-  this.title=title;
-  this.author=author;
-  this.numPages=numPages;
-  this.read=read;
-  this.library=library;
-}
-
-Book.prototype.bookElem = function () {
-  const bookcopy = document.querySelector(".book");
-  const booknode = bookcopy.cloneNode(true);
-  this.bookelem=booknode;
-  booknode.classList.remove("hidden");
-  booknode.classList.add("seen")
-  const bookstring = [this.title, "by " + this.author, this.numPages + (this.numPages == 1 ? " pg" : " pgs")];
-  for (i = bookstring.length; i >= 0; i--){
-    textnode=document.createTextNode(bookstring[i]);
-    breaknode=document.createElement("br");
-    booknode.insertBefore(textnode, booknode.firstChild);
-    booknode.insertBefore(breaknode, booknode.firstChild);
+  constructor(user) {
+    this.user=user;
+    this.setupUI();
   }
-  booknode.classList.add(this.read ? "read" : null);
-  this.readbutton=booknode.children[4].children[0];
-  this.readbutton.addEventListener("click", (event) => {
-    this.bookelem.classList.contains("read") ? this.bookelem.classList.remove("read") : this.bookelem.classList.add("read");
-    event.preventDefault();
-  });
-  this.deletebutton=booknode.children[4].children[1];
-  this.deletebutton.addEventListener("click", (event) => {
-    this.library.removeBookFromLibrary(this);
-    event.preventDefault();
-  })
-  return booknode;
+
+  setupUI() {
+    //Set formatting and event listeners
+    BookshelfUI.libraryheader.innerHTML=this.user +"'s Library";
+    BookshelfUI.showform.addEventListener("click", () => {
+      BookshelfUI.addbookform.classList.remove("hidden")
+    });
+    BookshelfUI.closeform.addEventListener("click", (event) => {
+      BookshelfUI.addbookform.classList.add("hidden");
+      event.preventDefault();
+    });
+    BookshelfUI.addbook.addEventListener("click", (event) => {
+      let inputarray=Array.from(BookshelfUI.inputfields);
+      let bookargs=inputarray.map((node) => node.id !== "readcheck" ? node.value : node.checked);
+      const book=new Book(bookargs[0], bookargs[1], bookargs[2], bookargs[3], this);
+      this.addBookToLibrary(book);
+      BookshelfUI.addbookform.reset();
+      event.preventDefault();
+    }); 
+  }
+
+  addBookToLibrary(book) {
+    this.bookshelfarr.push(book);
+    BookshelfUI.bookshelfUI.appendChild(book.bookelem);
+  }
+
+  removeBookFromLibrary(book) {
+    const index = this.bookshelfarr.indexOf(book);
+    const splicedarray = this.bookshelfarr.splice(index, 1);
+    this.bookshelfarr = splicedarray;
+    BookshelfUI.bookshelfUI.removeChild(book.bookelem);
+  }
 }
 
-Bookshelf.prototype.addBookToLibrary = function (book) {
-  this.bookshelf.push(book);
-  this.bookshelfelem.appendChild(book.bookElem());
-}
+class Book {
 
-Bookshelf.prototype.removeBookFromLibrary = function (book) {
-  const index = this.bookshelf.indexOf(book);
-  const splicedarray= this.bookshelf.splice(index, 1);
-  this.bookshelf = splicedarray;
-  this.bookshelfelem.removeChild(book.bookelem);
+  static bookUI = $bookcopy;
+  bookelem;
+  
+  constructor(title, author, numPages, read, library) {
+    this.title=title;
+    this.author=author;
+    this.numPages=numPages;
+    this.read=read;
+    this.library=library;
+    this.createBookElem();
+  }
+
+  createBookElem() {
+    this.bookelem=Book.bookUI.cloneNode(true);
+    this.bookelem.classList.remove("hidden");
+    this.bookelem.classList.add("seen");
+    const bookstring = [this.title, "by " + this.author, this.numPages 
+                        + (this.numPages == 1 ? " pg" : " pgs")];
+    for (let i = bookstring.length-1; i >= 0; i--){
+      const textnode=document.createTextNode(bookstring[i]);
+      const breaknode=document.createElement("br");
+      this.bookelem.insertBefore(textnode, this.bookelem.firstChild);
+      this.bookelem.insertBefore(breaknode, this.bookelem.firstChild);
+    }
+    this.bookelem.classList.add(this.read ? "read" : null);
+    const readbutton=this.bookelem.children[3].children[0];
+    readbutton.addEventListener("click", (event) => {
+      this.bookelem.classList.contains("read") ? this.bookelem.classList.remove("read") : 
+                                                 this.bookelem.classList.add("read");
+      event.preventDefault();
+    });
+    const deletebutton=this.bookelem.children[3].children[1];
+    deletebutton.addEventListener("click", (event) => {
+      this.library.removeBookFromLibrary(this);
+      event.preventDefault();
+    })
+  }
+
 }
 
 function main() {
+  newbookshelf=new BookshelfUI("Cabruh");
 }
-
 main();
